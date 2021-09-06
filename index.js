20c49ab6f221f688aec2fd7aac0724a59c1bf584
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-09-07 02:26:20 +0300

    no logic switch start/stop

--- index.js
+++ index.js
@@ -12,62 +12,131 @@ refs.toggleWotch.addEventListener("click", startTime);
 refs.resetBtn.addEventListener("click", reset);
 refs.pauseBtn.addEventListener("click", pause);
 
-let currentSecs = 0;
-let currentMins = 0;
-let currentHours = 0;
-let interval;
+const obj = {
+  currentSecs: 57,
+  currentMins: 59,
+  currentHours: 0,
+  interval: null,
+};
 
 function setCountUpTimer() {
-  interval = setInterval(() => {
-    currentSecs++;
-    refs.secs.textContent = currentSecs;
+  refs.toggleWotch.setAttribute("is_stop", "stop");
+  changeTextBtn("Stop");
 
-    if (currentSecs === 60) {
-      currentSecs = 0;
-      currentMins++;
-      refs.mins.textContent = currentMins;
+  obj.interval = setInterval(() => {
+    obj.currentSecs++;
+    refs.secs.innerHTML = obj.currentSecs;
+    refs.mins.textContent = obj.currentMins;
+    refs.hours.textContent = obj.currentHours;
+
+    if (obj.currentSecs === 60) {
+      obj.currentSecs = 0;
+      obj.currentMins++;
     }
 
-    if (currentMins === 60) {
-      currentSecs = 0;
-      currentMins = 0;
-      currentHours++;
-      refs.hours.textContent = currentHours;
+    if (obj.currentMins === 60) {
+      obj.currentSecs = 0;
+      obj.currentMins = 0;
+      obj.currentHours++;
     }
   }, 1000);
 }
 
 function startTime(e) {
-  e.preventDefault();
-  console.log(e.target);
-
-  if (e.target) {
-    setCountUpTimer();
-  }
+  setCountUpTimer();
 }
 
-// function start(currentSecs) {
-//     if (currentSecs === 60) {
-//       currentMins++;
-//       refs.mins.textContent = currentMins;
-//     }
+// function r() {
+//   const s = document.body.querySelector("button[is_stop='stop']");
+//   // .addEventListener("click", fn);
+//   s.addEventListener("click", reset());
+//   console.log(s);
 // }
 
-function pause(e) {
-  if (e.target) {
-    clearInterval(interval);
-  }
+function pause() {
+  clearInterval(obj.interval);
+  changeTextBtn("Start");
 }
 
-function reset(e) {
-  if (e.target) {
-    currentSecs = 0;
-    currentMins = 0;
-    currentHours = 0;
-    refs.secs.textContent = "00";
-    refs.mins.textContent = "00";
-    refs.hours.textContent = "00";
+function reset() {
+  obj.currentSecs = 0;
+  obj.currentMins = 0;
+  obj.currentHours = 0;
+  refs.secs.textContent = "0";
+  refs.mins.textContent = "0";
+  refs.hours.textContent = "0";
 
-    clearInterval(interval);
-  }
+  clearInterval(obj.interval);
 }
+
+function changeTextBtn(text) {
+  refs.toggleWotch.textContent = text;
+}
+
+// const refs = {
+//   toggleWotch: document.querySelector("#toggleWotch"),
+//   pauseBtn: document.querySelector("#pauseBtn"),
+//   resetBtn: document.querySelector("#resetBtn"),
+
+//   time: document.querySelector('[data-value="time"]'),
+// };
+
+// refs.toggleWotch.addEventListener("click", startTimer);
+// refs.resetBtn.addEventListener("click", reset);
+// refs.pauseBtn.addEventListener("click", pause);
+
+// let interval;
+
+// const padStrStart = (num) => String(num).padStart(2, 0);
+
+// const getUnits = (ms) => {
+//   const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+//   return { hours, minutes, seconds };
+// };
+
+// const renderTime = (time) => {
+//   let { hours, minutes, seconds } = time;
+//   hours = padStrStart(hours);
+//   minutes = padStrStart(minutes);
+//   seconds = padStrStart(seconds);
+//   refs.time.textContent = `${hours} hours / ${minutes} minutes / ${seconds} seconds`;
+// };
+
+// const getTime = (startTime) => {
+//   const now = new Date();
+//   const diff = now - startTime;
+//   const timeObj = getUnits(diff);
+//   renderTime(timeObj);
+// };
+
+// function startTimer() {
+//   refs.toggleWotch.removeAttribute("is_stop", "Stop");
+//   changeTextBtn("Start");
+
+//   const startTime = new Date();
+//   getTime(startTime);
+
+//   interval = setInterval(() => {
+//     getTime(startTime);
+
+//     changeTextBtn("Stop");
+//   }, 1000);
+// }
+
+// function pause() {
+//   refs.toggleWotch.setAttribute("is_stop", "Stop");
+//   changeTextBtn("Start");
+//   clearInterval(interval);
+// }
+
+// function reset() {
+//   refs.time.textContent = "00 hours / 00 minutes / 00 seconds";
+//   changeTextBtn("Start");
+//   clearInterval(interval);
+// }
+
+// function changeTextBtn(text) {
+//   refs.toggleWotch.textContent = text;
+// }
